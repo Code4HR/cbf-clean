f86f76eaff4949000e7c536acdd0641dec8a54d7
Bret Fisher <bret@fishbrains.com>
2013-02-06 20:50:53 -0500

    meetup work, making form modal

--- cbdZoneReport.js
+++ cbdZoneReport.js
@@ -2,7 +2,27 @@ Partners = new Meteor.Collection("partners");
 ZoneReports = new Meteor.Collection("zoneReports");
 
 if (Meteor.isClient) {
-  Template.hello.partners = function () {
+
+///////////////////////////////////////////////////////////////////////////////
+// Create Report dialog
+
+  var openCreateDialog = function () {
+  Session.set("createError", null);
+  Session.set("showCreateDialog", true);
+};
+
+Template.hello.showCreateDialog = function () {
+  return Session.get("showCreateDialog");
+};
+
+Template.createDialog.error = function () {
+  return Session.get("createError");
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// form filling
+
+  Template.createDialog.partners = function () {
     return Partners.find({}, {sort: {name: 1}});
   };
 
@@ -10,39 +30,33 @@ Template.hello.zoneReports = function () {
     return ZoneReports.find({}, {sort: {zoneCaptain: 1}});
   };
 
-  Template.hello.events({
-    'click input.btn' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        // console.log("You pressed the button");
-        // console.log(document.getElementById("zoneCaptain").value);
-        // console.log(document.getElementById("partner").value);
-        // console.log(document.getElementById("zone").value);
-        // console.log(document.getElementById("volunteers").value);
-        // console.log(document.getElementById("activeMilitary").value);
-        // console.log(document.getElementById("boats").value);
-        // console.log(document.getElementById("poundsCollected").value);
-        // console.log(document.getElementById("milesCleaned").value);
-        // console.log(document.getElementById("mostUnusualItem").value);
-        // console.log(document.getElementById("mostCommonItem").value);
-        // console.log(document.getElementById("largestItem").value);
-        // console.log(document.getElementById("trashPickup").value);
-        // console.log(document.getElementById("picknic").value);
-        ZoneReports.insert({
-                  zoneCaptain: document.getElementById("zoneCaptain").value,
-                  partner: document.getElementById("partner").value,
-                  zone: document.getElementById("zone").value,
-                  volunteers: document.getElementById("volunteers").value,
-                  activeMilitary: document.getElementById("activeMilitary").value,
-                  boats: document.getElementById("boats").value,
-                  poundsCollected: document.getElementById("poundsCollected").value,
-                  milesCleaned: document.getElementById("milesCleaned").value,
-                  mostUnusualItem: document.getElementById("mostUnusualItem").value,
-                  mostCommonItem: document.getElementById("mostCommonItem").value,
-                  largestItem: document.getElementById("largestItem").value,
-                  trashPickup: document.getElementById("trashPickup").value,
-                  picknic: document.getElementById("picknic").value
-        });
+Template.hello.events({
+  'click .btn-large' : function () {
+    openCreateDialog();
+  }
+});
+
+Template.createDialog.events({
+  'click .save' : function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the button");
+      console.log(document.getElementById("zoneCaptain").value);
+      ZoneReports.insert({
+                zoneCaptain: document.getElementById("zoneCaptain").value,
+                partner: document.getElementById("partner").value,
+                zone: document.getElementById("zone").value,
+                volunteers: document.getElementById("volunteers").value,
+                poundsCollected: document.getElementById("poundsCollected").value,
+                milesCleaned: document.getElementById("milesCleaned").value,
+                mostUnusualItem: document.getElementById("mostUnusualItem").value,
+                mostCommonItem: document.getElementById("mostCommonItem").value,
+                largestItem: document.getElementById("largestItem").value,
+                activeMilitary: document.getElementById("activeMilitary").value,
+                boats: document.getElementById("boats").value,
+                trashPickup: document.getElementById("trashPickup").value,
+                picknic: document.getElementById("picknic").value
+      });
     }
   });
 }
