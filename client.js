52b4aa9d4bad383b1d1c03d9e6c259e52c010fbc
Bret Fisher <bret@fishbrains.com>
2013-02-25 17:49:24 -0500

    switching to saving csv on server

--- client.js
+++ client.js
@@ -97,8 +97,33 @@ Template.reportMain.events({
     // but will likely suck with a thousand records, so it'll need to be done 
     // server-side and sent down
 
-// get keys
-var data = ZoneReports.find({}, {sort: {partner: 1}}).fetch();
+    // get keys
+    var data = ZoneReports.find({}, {sort: {partner: 1}}).fetch();
+    var keys = _.keys(data[0]);
+     
+    // convert to csv string
+    var csv = keys.join(",");
+    _(data).each(function(row) {
+      csv += "\n";
+      csv += _(keys).map(function(k) {
+        return row[k];
+      }).join(",");
+    });
+
+    //openDataURL(createDataURL(data));
+
+    // trick browser into downloading file
+    var uriContent = "data:application/octet-stream;base64," + encodeURIComponent(csv);
+    var myWindow = window.open(uriContent, "Reports Download");
+    myWindow.focus();
+
+    // object to array
+    var reports = ZoneReports.find({}, {sort: {partner: 1}}).fetch();
+    var csv;
+
+    //array to json
+    csv = JSON.stringify(reports);
+    console.log(csv);
   }
 });
 
@@ -137,6 +162,8 @@ Template.createDialog.events({
       });
     }
     closeCreateForm();
+        Meteor.call('saveFile', callback);
+
     }
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,46 @@
+Meteor.startup(function () {
+    var require = __meteor_bootstrap__.require;
+    var path = require('path');
+    var fs = require('fs');
+    var base = path.resolve('.');
+    var isBundle = fs.existsSync(base + '/bundle');
+    var modulePath = base + (isBundle ? '/bundle/static' : '/public') + '/node_modules';
+
+    var csv = require(modulePath + '/csv');
+});
+
+Meteor.methods({
+  saveFile: function() {
+    var path = cleanPath(path), fs = __meteor_bootstrap__.require('fs'),
+      name = cleanName(name || 'file'), encoding = encoding || 'binary',
+      chroot = Meteor.chroot || 'public';
+    // Clean up the path. Remove any initial and final '/' -we prefix them-,
+    // any sort of attempt to go to the parent directory '..' and any empty directories in
+    // between '/////' - which may happen after removing '..'
+    path = chroot + (path ? '/' + path + '/' : '/');
+    
+    // TODO Add file existance checks, etc...
+    fs.writeFile(path + name, blob, encoding, function(err) {
+      if (err) {
+        throw (new Meteor.Error(500, 'Failed to save file.', err));
+      } else {
+        console.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
+      }
+    }); 
+ 
+    function cleanPath(str) {
+      if (str) {
+        return str.replace(/\.\./g,'').replace(/\/+/g,'').
+          replace(/^\/+/,'').replace(/\/+$/,'');
+      }
+    }
+    function cleanName(str) {
+      return str.replace(/\.\./g,'').replace(/\//g,'');
+    }
+  }
+});
+
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
