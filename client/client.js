b33b4623e6b092267edd8aab4fa5cde2782083db
Bret Fisher <me@bfish.us>
2013-04-01 01:12:14 -0400

    fixed #25 form popups for calculations

--- client/client.js
+++ client/client.js
@@ -49,6 +49,23 @@ Template.zoneForm.partners = function () {
   return Partners.find({}, {sort: {name: 1}});
 };
 
+Template.zoneForm.rendered = function () {
+  $('#poundsCollected').popover({
+    title: "Estimated weidghts for debris collected",
+    html: true,
+    trigger: "focus",
+    placement: "right",
+    content: "Automobile parts:25 lbs.<br/>Metal container:50 lbs.<br/>Bag of litter:15-25 lbs.<br/>Wooden pallet:30 lbs.<br/>Bag of clothes:30 lbs.<br/>Refrigerator:500 lbs.<br/>Bicycle:25 lbs.<br/>Shopping cart:50-60 lbs.<br/>Chair:25 lbs.<br/>Sofa:80 lbs.<br/>Dishwasher:75 lbs.<br/>TV/Computer:15 lbs.<br/>Dresser:50 lbs.<br/>Tire:25-50 lbs.<br/>Mattress:25 lbs."
+  });
+  $('#milesCleaned').popover({
+    title: "Estimating Distance to be Cleaned",
+    html: true,
+    trigger: "focus",
+    placement: "right",
+    content: "Estimate the longest linear length (i.e., from point A to point B along the beach/shore) to be cleaned by the volunteers. Do not report area measurements, such as square feet/miles. Use the following method to convert area measurements to linear distances: Number of square feet cleaned divided by 10 ft.* divided by 5280 ft. (1 mile) = linear miles cleaned *10 feet represents an average linear path cleaned by a volunteer (5 feet on each side of the person)<br/><br/>Example 1: You plan to clean a 1-acre area. 1 acre = 43,560 ft2 43,560 ÷ 10 ÷ 5280 = 0.825 miles cleaned<br/><br/> Example 2: You plan to clean 5000 ft2 . 5000 ÷ 10 ÷ 5280 = 0.094 miles"
+  });
+};
+
 Template.reportMain.partners = function () {
   return Partners.find({}, {sort: {name: 1}});
 };
